375f6cbf17442935bc451b85cbe4bed631825f5e
neharepos <nehakeshrimail@gmail.com>
2026-01-29 11:32:27 +0530

    made the view all products button work

--- components/Products/Products.tsx
+++ components/Products/Products.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Img1 from "../../public/women/women1.jpg";
 import Img2 from "../../public/women/women2.jpg";
 import Img3 from "../../public/women/womenn3.jpeg";
@@ -55,65 +55,77 @@ const ProductsData = [
   },
 ];
 
-
-
 const Products = () => {
   return (
-    <div className='mt-14 mb-12'>
-        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-75'>
-            {/* Header section */}
-            <div className='text-center mb-10 max-w-150 '>
-                <p data-aos="fade-up" className='text-sm text-orange-500'>Top Selling Products for you</p>
-                <h1 data-aos="fade-up" className='text-3xl font-bold'>Products</h1>
-                <p data-aos="fade-up" className='text-xs text-gray-400'>Lorem ipsum dolor sit amet consectetur 
-                    adipisicing elit. Ea, expedita.</p>
-            </div>
-            {/* body section */}
-            <div>
-                <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4
-                lg:grid-cols-5 place-items-center gap-5'>
-                    {/* card section */}
-                    {
-                        ProductsData.map((data) => (
-                              <Link 
-    href={`/Products/${data.category}`} 
-    key={data.id}
-    className="cursor-pointer" // Changes the mouse to a hand icon
-  >
-                            <div 
-                            data-aos="fade-up"
-                            data-aos-delay={data.aosDelay}
-                            key={data.id} 
-                            className='space-y-3'>
-                                <img src={data.img.src} alt=""
-                                className='h-55 w-37.5 object-cover rounded-md'
-                                />
-                                <div>
-                                    <h3 className='font-semibold'>{data.title}</h3>
-                                    <p className='text-sm text-gray-600'>{data.color}</p>
-                                
-                                <div className='flex items-center gap-1'>
-                                    <IoStarSharp className='text-yellow-400' />
-                                    <span>{data.rating}</span>
-                                </div>
-                                </div> 
-                            </div>
-                            </Link>
-                        ))
-                    }
-                </div>
-                
-                {/* view all button */}
-                <div className='flex justify-center'>
-                  <button className='text-center mt-10
-                  cursor-pointer bg-orange-400 text-white py-1 
-                  px-5 rounded-md'>View All Button</button>
-                </div>
-            </div>
+    <div className="mt-14 mb-12">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-75">
+        {/* Header section */}
+        <div className="text-center mb-10 max-w-150 ">
+          <p data-aos="fade-up" className="text-sm text-orange-500">
+            Top Selling Products for you
+          </p>
+          <h1 data-aos="fade-up" className="text-3xl font-bold">
+            Products
+          </h1>
+          <p data-aos="fade-up" className="text-xs text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea,
+            expedita.
+          </p>
         </div>
-    </div>
-  
-  )
-}
+        {/* body section */}
+        <div>
+          <div
+            className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4
+                lg:grid-cols-5 place-items-center gap-5"
+          >
+            {/* card section */}
+            {ProductsData.map((data) => (
+              <Link
+                href={`/Products/${data.category}`}
+                key={data.id}
+                className="cursor-pointer" // Changes the mouse to a hand icon
+              >
+                <div
+                  data-aos="fade-up"
+                  data-aos-delay={data.aosDelay}
+                  key={data.id}
+                  className="space-y-3"
+                >
+                  <img
+                    src={data.img.src}
+                    alt=""
+                    className="h-55 w-37.5 object-cover rounded-md"
+                  />
+                  <div>
+                    <h3 className="font-semibold">{data.title}</h3>
+                    <p className="text-sm text-gray-600">{data.color}</p>
 
-export default Products
+                    <div className="flex items-center gap-1">
+                      <IoStarSharp className="text-yellow-400" />
+                      <span>{data.rating}</span>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+
+          {/* view all button */}
+          <div className="flex justify-center">
+            <Link href="/Products">
+            <button
+              className="text-center mt-10
+                  cursor-pointer bg-orange-400 text-white py-1 
+                  px-5 rounded-md"
+            >
+              View All Button
+            </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
